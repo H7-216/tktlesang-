19f25604bb529ff181eef9af99e1bcfed828b857
H7-216 <hmoussa006@gmail.com>
2024-11-11 00:58:19 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ function displayResults(movies) {
         const movieElement = document.createElement('div');
         movieElement.classList.add('movie');
         movieElement.innerHTML = `
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title} Poster">
             <h2>${movie.title}</h2>
             <p>${movie.overview}</p>
             <p><strong>Note :</strong> ${movie.vote_average}</p>
+            <a href="https://www.themoviedb.org/movie/${movie.id}" target="_blank">Voir plus</a>
         `;
         results.appendChild(movieElement);
     });
